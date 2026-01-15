703d5e75de133cce3eff2f6ffa47cbeaf7d2be4e
otishiPuspi <otishy52.ist@gmail.com>
2026-01-15 13:01:18 +0600

    java script practice

---
+++ java/script.js
@@ -0,0 +1,66 @@
+console.log('1.Addition');
+let a,b,c;
+a=10;
+b=5;
+c=(a+b);
+console.log('Add first number a =',10);
+console.log('Add second number b =',5);
+console.log('The sum of a and b is, c =',c);
+
+console.log(".............................");
+
+console.log("2.Subtraction");
+let d,e,f;
+d=20;
+e=10;
+f=(d-e);
+console.log("Add first number d =",20);
+console.log("Add second number e =",10);
+console.log('The sub of d and e is, f =',f);
+
+console.log(".............................");
+
+console.log("3.Multiplication");
+let g,h,i;
+g=5;
+h=5;
+i=(g*h);
+console.log("Add first number g =",5);
+console.log("Add second number h =",5);
+console.log('The mul of g and h is , i =',i);
+
+console.log(".............................");
+
+console.log("4.Division");
+let j,k,l;
+j=20;
+k=5;
+l=(j/k);
+console.log("Add first number j =",20);
+console.log("Add second number k =",5);
+console.log('The div of j and k is ,l  =',l);
+
+
+console.log(".............................");
+
+console.log("5.Remainder");
+let m,n,o;
+m=11;
+n=5;
+o=(m%n);
+console.log("Add first number m =",11);
+console.log("Add second number n =",5);
+console.log('The remainder of m and n is ,o  =',o);
+
+console.log(".............................");
+
+console.log("6.Power");
+let p,q,r;
+p=3;
+q=3;
+r=(p**q);
+console.log("Add the base p =",3);
+console.log("Add the power q =",3);
+console.log('The result of 3 to the power 3 is ,r  =',r);
+
+console.log(".............................");
