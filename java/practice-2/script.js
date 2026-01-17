d0a6f3fd2a8645d9989587ff4d1999166c3ca454
otishiPuspi <otishy52.ist@gmail.com>
2026-01-17 10:28:29 +0600

    Practice-2

---
+++ java/practice-2/script.js
@@ -0,0 +1,31 @@
+document.getElementById("btn").onclick=function(){
+  let username;
+  username=document.getElementById("username").value;
+  console.log(username);
+}
+
+let name="Otishi Islam Puspi";
+let age=22;
+let IsStudent= true;
+let hobbies =["Learning new things✨","Drawing 🎨","Travelling 🏞","Photographyies 📸"];
+let myAddress={Street:"Board Bazar", City:"Gazipur",Country:"Bangladesh"};
+
+console.log("About Me: 💁‍♀️");
+
+console.log(         );
+console.log("My name is:",name);
+console.log("Age:",age);
+console.log("Am i a student?:",IsStudent);
+console.log("My Hobbies Are:",hobbies);
+console.log("My first hobby is:", hobbies[0]);
+console.log("My second hobby is:", hobbies[1]);
+console.log("My third hobby is:", hobbies[2]);
+console.log("My fourth hobby is:", hobbies[3]);
+console.log("My Address:",myAddress);
+console.log("Street:",myAddress.Street);
+console.log("City:",myAddress.City);
+console.log("Country:",myAddress.Country);
+
+
+
+
