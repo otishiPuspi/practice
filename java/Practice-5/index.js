1976aa3dc80f69931efa6c0a689f35945ee1587b
otishiPuspi <otishy52.ist@gmail.com>
2026-01-27 12:39:37 +0600

    Array-practice

---
+++ java/Practice-5/index.js
@@ -0,0 +1,34 @@
+
+//Remove 1st element
+let array = [10,20,30,40];
+console.log(array);
+array.shift();
+console.log(array);
+
+//Add number at end of the array
+let arr = [1,2,3,4];
+console.log(arr);
+arr.pop();
+console.log(arr);
+
+//Reverse the order of array
+let ar = [5,6,7,8];
+console.log(ar);
+console.log(ar.reverse());
+
+//Sort this array in ascending order: [9, 3, 7, 1]
+let a = [9,3,7,1];
+console.log(a);
+console.log(a.sort());
+
+// Add 0 at the beginning of this array: [2, 4, 6, 8]
+let b = [2,4,6,8];
+console.log(b);
+b.unshift(0);
+console.log(b);
+
+// Remove the last element from this array: [11, 22, 33, 44]
+let c = [11,22,33,44];
+console.log(c);
+c.pop();
+console.log(c);
